72d1480cf6212fc0a41d2ae95b36977cc0b90f64
richdouglasevans <rich.douglas.evans@gmail.com>
2025-05-04 15:33:59 +0100

    Fullscreen published stories
    Closes #100

--- src/components/story/published/PublishedStory.tsx
+++ src/components/story/published/PublishedStory.tsx
@@ -1,6 +1,6 @@
 import { Hud, OrbitControls, PerspectiveCamera } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
-import { FontFamilyProvider } from "@react-three/uikit";
+import { FontFamilyProvider, Root } from "@react-three/uikit";
 import type { ResourceKey } from "i18next";
 import { type FC, useState } from "react";
 import { I18nextProvider } from "react-i18next";
@@ -15,10 +15,12 @@ import type {
 } from "@domain/story/publish/types.js";
 import useI18N from "@i18n/client/useI18N.js";
 
+import htmlIdForStory from "../htmlIdForStory.js";
 import Skybox from "./Skybox/Skybox.js";
 import BackgroundSound from "./audio/BackgroundSound.js";
 import Fiction from "./fiction/Fiction.js";
 import ExitStory from "./support/ExitStory.js";
+import GoFullscreen from "./support/GoFullscreen.js";
 import changePage from "./support/changePage.js";
 
 type PublishedStoryViewProps = {
@@ -50,10 +52,18 @@ const PublishedStoryView: FC<PublishedStoryViewProps> = ({ story }) => {
     };
   })();
 
+  const onExitStory = () => {
+    window.location.href = new URL("/story", window.location.origin).href;
+  };
+
   return (
     <Canvas
+      id={htmlIdForStory(story.id)}
+      gl={{
+        localClippingEnabled: true,
+        /* for fullscreening */ preserveDrawingBuffer: true,
+      }}
       style={{ position: "absolute", inset: "0" }}
-      gl={{ localClippingEnabled: true }}
     >
       <PerspectiveCamera makeDefault position={[0, 0, 5]} />
 
@@ -68,8 +78,19 @@ const PublishedStoryView: FC<PublishedStoryViewProps> = ({ story }) => {
         <group position={[-3, 0, 1]}>
           <Fiction content={currentPage.content} followLink={followLink} />
         </group>
-        <group position={[5, -2.5, 1]}>
-          <ExitStory />
+
+        <group position={[4.5, -2.5, 1]}>
+          <Root
+            sizeX={1}
+            gapRow={3}
+            flexDirection="column"
+            positionType={"static"}
+            positionTop={0}
+            positionLeft={0}
+          >
+            <GoFullscreen />
+            <ExitStory onExit={onExitStory} />
+          </Root>
         </group>
       </Hud>
     </Canvas>
