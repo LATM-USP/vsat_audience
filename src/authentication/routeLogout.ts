76c372a5edaee4f1c0890f2350d0263cdef78821
richdouglasevans <rich.douglas.evans@gmail.com>
2025-08-27 10:36:28 +0100

    Session is now just 1 day long
    'cos students re-use lab equipment.

--- src/authentication/routeLogout.ts
+++ src/authentication/routeLogout.ts
@@ -10,12 +10,21 @@ export default function logoutRoute(log: Logger): Router {
 
       res.redirect(locationAfterLogout);
     } else {
-      log.info({ user: req.user, req }, "Logging out user");
+      log.debug({ user: req.user, req }, "Logging out user");
 
       req.logout((err) => {
         if (err) {
           log.error({ err, user: req.user }, "Error logging out user");
         }
+
+        req.session.destroy((errSession) => {
+          if (errSession) {
+            log.warn({ err: errSession, user: req.user, req },
+              "Error destroying HTTP session on logout");
+          }
+        })
+
+        log.info({ user: req.user, req }, "User successfully logged out");
 
         res.redirect(locationAfterLogout);
       });
